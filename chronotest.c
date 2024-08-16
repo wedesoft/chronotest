777192761848359c665aa71cac70091ce12cfb32
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-08-16 20:12:36 +0100

    Render a cube

--- chronotest.c
+++ chronotest.c
@@ -1,8 +1,104 @@
+#include <math.h>
+#include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int width = 640;
 int height = 480;
+
+const char *vertexSource = "#version 410 core\n\
+uniform float aspect;\n\
+in vec3 point;\n\
+in vec3 normal;\n\
+out vec3 n;\n\
+void main()\n\
+{\n\
+  n = normal;\n\
+  gl_Position = vec4(point * vec3(1, aspect, 1), 1);\n\
+}";
+
+const char *fragmentSource = "#version 410 core\n\
+uniform vec3 light;\n\
+in vec3 n;\n\
+out vec3 fragColor;\n\
+void main()\n\
+{\n\
+  float ambient = 0.4;\n\
+  float diffuse = 0.6 * max(dot(light, n), 0);\n\
+  fragColor = vec3(1, 1, 1) * (ambient + diffuse);\n\
+}";
+
+// Vertex array data
+GLfloat vertices[] = {
+  // Front face
+  -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+   0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+   0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+  -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+  // Back face
+  -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+   0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+   0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+  -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+  // Left face
+  -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+  -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+  -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+  -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+
+  // Right face
+   0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+   0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+   0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+   0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+
+  // Top face
+  -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+   0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+   0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+  -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+
+  // Bottom face
+  -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+   0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+   0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+  -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f
+};
+
+unsigned int indices[] = {
+   0,  1,  2,  3,
+   4,  5,  6,  7,
+   8,  9, 10, 11,
+  12, 13, 14, 15,
+  16, 17, 18, 19,
+  20, 21, 22, 23
+};
+
+void handleCompileError(const char *step, GLuint shader)
+{
+  GLint result = GL_FALSE;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+  if (result == GL_FALSE) {
+    char buffer[1024];
+    glGetShaderInfoLog(shader, 1024, NULL, buffer);
+    if (buffer[0])
+      fprintf(stderr, "%s: %s\n", step, buffer);
+  };
+}
+
+void handleLinkError(const char *step, GLuint program)
+{
+  GLint result = GL_FALSE;
+  glGetProgramiv(program, GL_LINK_STATUS, &result);
+  if (result == GL_FALSE) {
+    char buffer[1024];
+    glGetProgramInfoLog(program, 1024, NULL, buffer);
+    if (buffer[0])
+      fprintf(stderr, "%s: %s\n", step, buffer);
+  };
+}
 
 int main(void)
 {
@@ -11,11 +107,75 @@ int main(void)
   glfwMakeContextCurrent(window);
   glewInit();
 
+  glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+  glViewport(0, 0, width, height);
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexSource, NULL);
+  glCompileShader(vertexShader);
+  handleCompileError("Vertex shader", vertexShader);
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+  glCompileShader(fragmentShader);
+  handleCompileError("Fragment shader", fragmentShader);
+
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+  glLinkProgram(program);
+  handleLinkError("Shader program", program);
+
+  GLuint vao;
+  GLuint vbo;
+  GLuint idx;
+
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glGenBuffers(1, &idx);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glUseProgram(program);
+
+  glVertexAttribPointer(glGetAttribLocation(program, "point"),
+                        3, GL_FLOAT, GL_FALSE,
+                        6 * sizeof(float), (void *)0);
+  glVertexAttribPointer(glGetAttribLocation(program, "normal"),
+                        3, GL_FLOAT, GL_FALSE,
+                        6 * sizeof(float), (void *)(3 * sizeof(float)));
+
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glDisable(GL_CULL_FACE);
+  glEnable(GL_DEPTH_TEST);
+
+  float light[3] = {sqrt(0.5), sqrt(0.5), 0.0};
+  glUniform3fv(glGetUniformLocation(program, "light"), 1, light);
+  glUniform1f(glGetUniformLocation(program, "aspect"), (float)width / (float)height);
+
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glDrawElements(GL_QUADS, 24, GL_UNSIGNED_INT, (void *)0);
     glfwSwapBuffers(window);
     glfwPollEvents();
   };
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+  glDeleteBuffers(1, &idx);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glDeleteBuffers(1, &vbo);
+  glBindVertexArray(0);
+  glDeleteVertexArrays(1, &vao);
+
+  glDeleteProgram(program);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
   glfwTerminate();
   return 0;
