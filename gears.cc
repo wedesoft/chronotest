dab2ccea91d85bc6e4d55088c7f04204ddf5acca
Jan Wedekind (Dr) <jan@wedesoft.de>
2024-09-04 21:25:29 +0100

    Renamed some variables for cuboid rendering

--- gears.cc
+++ gears.cc
@@ -9,7 +9,7 @@
 int width = 1280;
 int height = 720;
 
-const char *vertexSource = "#version 410 core\n\
+const char *vertex_cuboid = "#version 410 core\n\
 uniform float aspect;\n\
 uniform vec3 axes;\n\
 uniform vec3 translation;\n\
@@ -23,7 +23,7 @@ void main()\n\
   gl_Position = vec4((rotation * (point * axes) + translation) * vec3(1, aspect, 1), 1);\n\
 }";
 
-const char *fragmentSource = "#version 410 core\n\
+const char *fragment_cuboid = "#version 410 core\n\
 uniform vec3 light;\n\
 in vec3 n;\n\
 out vec3 fragColor;\n\
@@ -35,7 +35,7 @@ void main()\n\
 }";
 
 // Vertex array data
-GLfloat vertices[] = {
+GLfloat vertices_cuboid[] = {
   // Front face
   -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
    0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
@@ -73,7 +73,7 @@ GLfloat vertices[] = {
   -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f
 };
 
-unsigned int indices[] = {
+unsigned int indices_cuboid[] = {
    0,  1,  2,  3,
    4,  5,  6,  7,
    8,  9, 10, 11,
@@ -116,21 +116,21 @@ int main(void)
   glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
   glViewport(0, 0, width, height);
 
-  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexSource, NULL);
-  glCompileShader(vertexShader);
-  handleCompileError("Vertex shader", vertexShader);
+  GLuint vertex_shader_cuboid = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader_cuboid, 1, &vertex_cuboid, NULL);
+  glCompileShader(vertex_shader_cuboid);
+  handleCompileError("Vertex shader", vertex_shader_cuboid);
 
-  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-  glCompileShader(fragmentShader);
-  handleCompileError("Fragment shader", fragmentShader);
+  GLuint fragment_shader_cuboid = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader_cuboid, 1, &fragment_cuboid, NULL);
+  glCompileShader(fragment_shader_cuboid);
+  handleCompileError("Fragment shader", fragment_shader_cuboid);
 
-  GLuint program = glCreateProgram();
-  glAttachShader(program, vertexShader);
-  glAttachShader(program, fragmentShader);
-  glLinkProgram(program);
-  handleLinkError("Shader program", program);
+  GLuint program_cuboid = glCreateProgram();
+  glAttachShader(program_cuboid, vertex_shader_cuboid);
+  glAttachShader(program_cuboid, fragment_shader_cuboid);
+  glLinkProgram(program_cuboid);
+  handleLinkError("Shader program", program_cuboid);
 
   GLuint vao;
   GLuint vbo;
@@ -141,17 +141,17 @@ int main(void)
 
   glGenBuffers(1, &vbo);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_cuboid), vertices_cuboid, GL_STATIC_DRAW);
   glGenBuffers(1, &idx);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices_cuboid), indices_cuboid, GL_STATIC_DRAW);
 
-  glUseProgram(program);
+  glUseProgram(program_cuboid);
 
-  glVertexAttribPointer(glGetAttribLocation(program, "point"),
+  glVertexAttribPointer(glGetAttribLocation(program_cuboid, "point"),
                         3, GL_FLOAT, GL_FALSE,
                         6 * sizeof(float), (void *)0);
-  glVertexAttribPointer(glGetAttribLocation(program, "normal"),
+  glVertexAttribPointer(glGetAttribLocation(program_cuboid, "normal"),
                         3, GL_FLOAT, GL_FALSE,
                         6 * sizeof(float), (void *)(3 * sizeof(float)));
 
@@ -162,13 +162,13 @@ int main(void)
   glEnable(GL_DEPTH_TEST);
 
   float light[3] = {0.36f, 0.8f, -0.48f};
-  glUniform3fv(glGetUniformLocation(program, "light"), 1, light);
-  glUniform1f(glGetUniformLocation(program, "aspect"), (float)width / (float)height);
+  glUniform3fv(glGetUniformLocation(program_cuboid, "light"), 1, light);
+  glUniform1f(glGetUniformLocation(program_cuboid, "aspect"), (float)width / (float)height);
   float a = 0.3;
   float b = 0.03;
   float c = 0.08;
   float axes[3] = {a, b, c};
-  glUniform3fv(glGetUniformLocation(program, "axes"), 1, axes);
+  glUniform3fv(glGetUniformLocation(program_cuboid, "axes"), 1, axes);
 
   chrono::ChSystemNSC sys;
   sys.SetTimestepperType(chrono::ChTimestepper::Type::RUNGEKUTTA45);
@@ -203,11 +203,11 @@ int main(void)
       (float)x.z(), (float)y.z(), (float)z.z()
     };
 
-    glUniformMatrix3fv(glGetUniformLocation(program, "rotation"), 1, GL_TRUE, rotation);
+    glUniformMatrix3fv(glGetUniformLocation(program_cuboid, "rotation"), 1, GL_TRUE, rotation);
 
     chrono::ChVector3 position = body->GetPos();
     float translation[3] = {(float)position.x(), (float)position.y(), (float)position.z()};
-    glUniform3fv(glGetUniformLocation(program, "translation"), 1, translation);
+    glUniform3fv(glGetUniformLocation(program_cuboid, "translation"), 1, translation);
 
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
     glDrawElements(GL_QUADS, 24, GL_UNSIGNED_INT, (void *)0);
@@ -224,9 +224,9 @@ int main(void)
   glBindVertexArray(0);
   glDeleteVertexArrays(1, &vao);
 
-  glDeleteProgram(program);
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
+  glDeleteProgram(program_cuboid);
+  glDeleteShader(vertex_shader_cuboid);
+  glDeleteShader(fragment_shader_cuboid);
 
   glfwTerminate();
   return 0;
